4d7031768b14a7edaf88266f16ad2519b3bf3dfa
yatsanf <frolnastya1980@gmail.com>
2026-05-02 01:32:42 +0300

    lab5

--- include/Document.hpp
+++ include/Document.hpp
@@ -3,12 +3,12 @@
 #include <cstdint>
 #include <string>
 
-class Document {
-public:
+class Document
+{
+  public:
     using Id = uint64_t;
 
-    Document(Id id, std::string name, std::string text)
-        : id_(id), name_(std::move(name)), text_(std::move(text)) {}
+    Document(Id id, std::string name, std::string text) : id_(id), name_(std::move(name)), text_(std::move(text)) {}
 
     Document(const Document&) = default;
     Document(Document&&) = default;
@@ -16,15 +16,25 @@ public:
     Document& operator=(Document&&) = default;
     ~Document() = default;
 
-    [[nodiscard]] Id id() const noexcept { return id_; }
-    [[nodiscard]] const std::string& name() const noexcept { return name_; }
-    [[nodiscard]] const std::string& text() const noexcept { return text_; }
+    [[nodiscard]] Id id() const noexcept
+    {
+        return id_;
+    }
+    [[nodiscard]] const std::string& name() const noexcept
+    {
+        return name_;
+    }
+    [[nodiscard]] const std::string& text() const noexcept
+    {
+        return text_;
+    }
 
-    [[nodiscard]] friend bool operator==(const Document& lhs, const Document& rhs) noexcept {
+    [[nodiscard]] friend bool operator==(const Document& lhs, const Document& rhs) noexcept
+    {
         return lhs.id_ == rhs.id_ && lhs.name_ == rhs.name_ && lhs.text_ == rhs.text_;
     }
 
-private:
+  private:
     Id id_;
     std::string name_;
     std::string text_;
